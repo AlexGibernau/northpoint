d21af95e4d70410836dfaa123998670cbb02a176
alex <alex.gibernau.castro@gmail.com>
2023-07-17 16:51:00 +0200

    auth

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from routers import products, users
-
+from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
 # Routers
@@ -8,6 +8,7 @@ app.include_router(products.router)
 
 app.include_router(users.router)
 
+app.mount("/static",StaticFiles(directory="static"),name="static")
 
 @app.get("/")
 async def root():
