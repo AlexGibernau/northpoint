57ef9239538554a5991c40bfc66649259040d039
alex <41009372g@iespoblenou.org>
2023-07-21 17:17:42 +0200

    terea finalizada

--- Tarea/tarea_end.py
+++ Tarea/tarea_end.py
@@ -17,17 +17,29 @@ buscador()
 
 response = requests.get(url_nueva)
 
+time_cue_out = None
+time_cue_in = None
+
 def busca_cue():
+    global time_cue_out, time_cue_in
     if "EXT-X-CUE-OUT:" in response.text:
+        if time_cue_out is None:  
+            time_cue_out = time.time()
         print("ENCONTRADO EL CUE OUT")
     elif "EXT-X-CUE-IN" in response.text:
+        if time_cue_in is None: 
+            time_cue_in = time.time()
         print("Encontrado el CUE IN")
     else:
         print("de momento nada")
 
+
 if __name__ == "__main__":
     while True:
         response = requests.get(url_nueva)
-        print(response.text)
         busca_cue()
+        if time_cue_out is not None and time_cue_in is not None:
+            tiempo_pasado = time_cue_in - time_cue_out
+            print(f"Tiempo entre CUE OUT y CUE IN: {tiempo_pasado:.2f} segundos")
+            break
         time.sleep(8)
