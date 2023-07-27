1ae2d51637d2e29ed6334b907b7328a6402d8163
alex <41009372g@iespoblenou.org>
2023-07-27 16:14:18 +0200

    tarea_2_acabada

--- Tarea/tarea_end_m3u8_V2.py
+++ Tarea/tarea_end_m3u8_V2.py
@@ -1,35 +1,43 @@
 import logging
-import time
 import sys
+import time
+
 import m3u8
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-
-master_url = "https://fast-tcseks01.thechannel.store/fast-channel-sin-filtros/fast-channel-sin-filtros.m3u8"
-logging.info("Start of cue out and cue in calculation")
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+master_url = sys.argv[1]
 master_m3u8 = m3u8.load(master_url)
+variant_selected = int(0)
+if len(sys.argv) > 2:
+    variant_selected = int(sys.argv[2])
+logging.info("Start of cue out and cue in calculation")
 
-variant0_url = master_m3u8.playlists[0].absolute_uri
+variant_url = master_m3u8.playlists[variant_selected].absolute_uri
+
+print(variant_url)
+
 
 def find_cue(variant_m3u8):
     global time_cue_out, time_cue_in
     if "EXT-X-CUE-OUT:" in variant_m3u8.dumps():
-        if time_cue_out is None:  
+        if time_cue_out is None:
             time_cue_out = time.time()
         logging.debug("EXT-X-CUE-OUT found")
     elif "EXT-X-CUE-IN" in variant_m3u8.dumps():
-        if time_cue_in is None: 
+        if time_cue_in is None:
             time_cue_in = time.time()
         logging.debug("EXT-X-CUE-IN found")
     else:
         logging.debug("not happened")
 
+
 time_cue_out = None
 time_cue_in = None
 
 while True:
-    variant_m3u8 = m3u8.load(variant0_url)
+    variant_m3u8 = m3u8.load(variant_url)
     find_cue(variant_m3u8)
     if time_cue_out is not None and time_cue_in is not None:
         tiempo_pasado = time_cue_in - time_cue_out
